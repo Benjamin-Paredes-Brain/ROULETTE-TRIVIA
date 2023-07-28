ced6bff095124dfcf6e34af9b166a9ec0781ec1d
Benjamin Paredes Brain <benja2005pb@gmail.com>
2023-07-27 22:54:31 -0300

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Roulette } from "./components/Roulette";
 import { QuestionsTrabajoInfantil } from "./components/Questions/QuestionsTrabajoIntantil";
 import { QuestionsHistoriaDelTrabajo } from "./components/Questions/QuestionsHistoriaDelTrabajo";
@@ -16,7 +16,7 @@ function App() {
           <Route path="/questions/historia-del-trabajo" element={<QuestionsHistoriaDelTrabajo />}/>
           <Route path="/questions/trabajo-infantil" element={<QuestionsTrabajoInfantil />} />
           <Route path="/questions/seguridad-social" element={<QuestionsSeguridadSocial />} />
-          <Route path="*" element={<h3>PAGINA NO ENCONTRADA</h3>} />
+          <Route path="*" element={<Navigate to="/roulette" />} />
         </Routes>
       </BrowserRouter>
     </>
